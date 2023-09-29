d68233383fa7a73239ae7d913e554cb25357fafa
Agung santoso <86614702+Agunxzzz@users.noreply.github.com>
2023-09-29 12:19:41 +0700

    Update updateyes.sh

--- update/updateyes.sh
+++ update/updateyes.sh
@@ -1,1 +1,11 @@
-echo "ga ada update bang"
+wget https://raw.githubusercontent.com/Agunxzzz/Mina-Xray-SSH/main/backup1.sh
+chmod 755 backup1.sh
+echo "tulis token botnya"
+read token
+sed -i -e s/initoken/"$token"/g /root/backup1.sh
+echo "tulis tuser idnya"
+read userid
+sed -i -e s/iniid/"$userid"/g /root/backup1.sh
+
+echo "2 23 * * * root /root/backup1.sh" >> /etc/crontab
+service cron restart
