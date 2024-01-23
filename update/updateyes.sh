04360c7dbcff9b68fe03562a44a84081315fe021
Agung santoso <86614702+Agunxzzz@users.noreply.github.com>
2024-01-23 11:58:43 +0700

    Update updateyes.sh

--- update/updateyes.sh
+++ update/updateyes.sh
@@ -1,14 +1,14 @@
-wget https://raw.githubusercontent.com/Agunxzzz/Mina-Xray-SSH/main/backup1.sh
-chmod 755 backup1.sh
-echo "tulis token botnya"
-read token
-sed -i -e s/initoken/"$token"/g /root/backup1.sh
-echo "tulis user idnya"
-read userid
-sed -i -e s/iniid/"$userid"/g /root/backup1.sh
+#wget https://raw.githubusercontent.com/Agunxzzz/Mina-Xray-SSH/main/backup1.sh
+#chmod 755 backup1.sh
+#echo "tulis token botnya"
+#read token
+#sed -i -e s/initoken/"$token"/g /root/backup1.sh
+#echo "tulis user idnya"
+#read userid
+#sed -i -e s/iniid/"$userid"/g /root/backup1.sh
 
-echo "2 23 * * * root /root/backup1.sh" >> /etc/crontab
-service cron restart
-rm versi
-touch versi
-echo "1.0.1" >> /root/versi
+#echo "2 23 * * * root /root/backup1.sh" >> /etc/crontab
+#service cron restart
+#rm versi
+#touch versi
+#echo "1.0.1" >> /root/versi
